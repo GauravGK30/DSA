dd5006454d3d6e34cfb3384ada8f0280fcee23ce
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-07-24 23:31:07 +0530

    Initial commit

---
+++ CPP Array and String/cppBasics/conditional.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //if-elseif-else
+    int x;
+    cin>>x;
+    // if(x<=10){
+    //     cout<<"small";
+    // }else if (x>10 && x<20)
+    // {
+    //     cout<<"medium";
+    // }else{
+    //     cout<<"big";
+    // }
+
+
+
+//switch case 
+    switch (x)
+    {
+    case 1:
+        cout<<"hii";
+        break;
+    case 2:
+        cout<<"bye";
+        break;
+    default:
+        cout<<"stop";
+        break;
+    }
+
+
+    
+    return 0;
+}
