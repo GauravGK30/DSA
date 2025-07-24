0ef316e3561a2940223fdacf2dc6e19d4f0e841f
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-07-24 23:17:37 +0530

    Initial commit

---
+++ CPP Array and String/cppBasics/cppBasics1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    std::cout<<"hii world";
+    cout<<"\n gk"<<endl;
+    int x,y;
+    cin >> x>> y;
+    cout<<"x"<<x<<" y"<<y;
+    return 0;
+}
---
+++ CPP Array and String/cppBasics/dataTypes.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    //int
+    int x=10;
+    //long
+    long a=1333;
+    //long long
+
+    long long y=2312;
+
+    //float,double;
+    float c=2.345;
+    double d=23;
+
+    //string and getline,Character
+    // string s;
+    // cin>>s;       //hi gk
+    // cout<<s;      // hi
+
+    string str;
+    getline(cin,str); //hi gk 
+    cout<<str<<endl;        //hi gk
+    
+    char ch='g';
+    cout<<ch;
+    
+    
+    return 0;
+}
