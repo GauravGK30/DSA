09aab9edab7d8476f0cf700484de29430bb726fa
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-08-03 00:09:36 +0530

    array

--- String/str2.cpp
+++ String/str2.cpp
@@ -3,35 +3,77 @@
 using namespace std;
 
 
+//palindrom
+bool isAlphaNum(char ch){
+    if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z')){
+        return true;
+    }
+    return false;
+}
+bool isPalindrom(string s){
+    
+    int st=0;
+    int end=s.size()-1;
+
+    while (st<end)
+    {
+        if(!isAlphaNum( s[st])){
+            st++;
+            continue;
+        }
+        if(!isAlphaNum( s[end])){
+            end--;
+            continue;
+        }
+        if(tolower(s[st]) != tolower(s[end])){
+            return false;
+            
+        }
+        st++;
+        end--;
+
+    }
+    return true;
+    
+}
+
+
 int main(){
 
-    string str1="gaurav ";
-    string str2="gk ";
+    // string str1="gaurav ";
+    // string str2="gk ";
 
-    string str3=str1+str2;
-    cout<<str3<<endl;
+    // string str3=str1+str2;
+    // cout<<str3<<endl;
 
-    cout<<(str1 == str2)<<endl;
-    cout<<(str1 < str2)<<endl;
-    cout<<str2.length()<<endl;
+    // cout<<(str1 == str2)<<endl;
+    // cout<<(str1 < str2)<<endl;
+    // cout<<str2.length()<<endl;
 
 
-    string str4;
-    // cin>>str4;
-    getline(cin,str4);
-    cout<<str4;
+    // string str4;
+    // // cin>>str4;
+    // getline(cin,str4);
+    // cout<<str4;
 
-    for(int i=0;i<str1.length();i++){
-        cout<<str1[i]<<" ";
-    }
+    // for(int i=0;i<str1.length();i++){
+    //     cout<<str1[i]<<" ";
+    // }
 
 
 
     // Reverse a string 
 
     string s;
-    getline(cin,s);
-    reverse(s.begin() , s.end());
-    cout<<s<<endl;
+    cin>>s;
+    
+    if (isPalindrom(s))
+        cout << s << " is a palindrome." << endl;
+    else
+        cout << s << " is not a palindrome." << endl;
+
+
+    // reverse(s.begin() , s.end());
+    // cout<<s<<endl;
     return 0;
 }
