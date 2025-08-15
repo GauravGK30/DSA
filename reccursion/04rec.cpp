35e9cd31bd0faf56575183a7281e230444b20bca
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-08-15 13:14:19 +0530

    recursion

---
+++ reccursion/04rec.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+//sort stack
+void insert(stack<int>& st,int temp){
+    if(st.size()==0 || st.top() <= temp){
+        st.push(temp);
+        return;
+    }
+    int val = st.top();
+    st.pop();
+    insert(st,temp);
+    st.push(val);
+    return;
+}
+
+void sortedStack(stack<int>& st){
+    if(st.size()==1){
+        return;
+    }
+    int temp= st.top();
+    st.pop();
+    sortedStack(st);
+    insert(st,temp);
+    return;
+}
+
+int main(){
+    stack<int> st ;
+    st.push(0);
+    st.push(2);
+    st.push(1);
+    st.push(5);
+
+    sortedStack(st);
+
+    // Print sorted stack (top to bottom)
+    while (!st.empty()) {
+        cout << st.top() << " ";
+        st.pop();
+    }
+    cout << endl;
+    return 0;
+
+}
