2fab76e7df55d11e020b8b52877421e6b2381f6b
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-08-16 15:57:44 +0530

    stl

--- STL/map.cpp
+++ STL/map.cpp
@@ -5,10 +5,13 @@ using namespace std;
 //key,value pair
 int main(){
     map<string,int> m;
-    // m[key] = value
+    // m[key] = value  //key sorted order print
     m["tv"] =10000;
     m["laptop"] =150000;
     for(auto p: m){
         cout<<p.first<<" "<<p.second<<endl;
     }
+    cout<<m.count("laptop");
+    cout<<m.erase("laptop");
+    //insert,emplace,count,erase,find
 }
