52b88c036804afe3d61042c17d339be1f552f952
Gaurav Kankuse <gauravkankuse18@gmail.com>
2025-09-18 22:03:26 +0530

    recurssion

--- reccursion/08rec.cpp
+++ reccursion/08rec.cpp
@@ -1,30 +1,36 @@
 #include<iostream>
 using namespace std;
-//all subsets
-void solve(string ip, string op){
-    if(ip.length()==0 ){
+
+void solve(string ip,string op){
+    if(ip.length()==0){
         cout<<op<<" ";
-        return;  //necessary;
+        return;
     }
 
     string op1 = op;
     string op2 = op;
 
+    op1.push_back(' ');
+    op1.push_back(ip[0]);
     op2.push_back(ip[0]);
+
     ip.erase(ip.begin() + 0);
 
     solve(ip,op1);
     solve(ip,op2);
-
     return;
 }
+
 
 int main(){
 
     string ip;
     cin>>ip;
 
-    string op = " ";
+    string op = "";
+
+    op.push_back(ip[0]);
+    ip.erase(ip.begin()+0);
     solve(ip,op);
 
 }
